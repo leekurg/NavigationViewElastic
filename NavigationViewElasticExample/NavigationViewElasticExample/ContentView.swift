f2fdef8c69c199b6d0709039235c524325ca729a
Ilya <105886145+leekurg@users.noreply.github.com>
2023-08-21 15:41:03 +0300

    AnyView -> @ViewBuilder content (#2)

--- NavigationViewElasticExample/NavigationViewElasticExample/ContentView.swift
+++ NavigationViewElasticExample/NavigationViewElasticExample/ContentView.swift
@@ -13,63 +13,48 @@ struct ContentView: View {
 
     var body: some View {
         NavigationViewElastic(
-            title: "Products",
-            blurStyle: .systemChromeMaterial,
-            content: content,
-            subtitleContent: subtitleContent,
-            primaryActionLabel: primaryActionLabel,
-            stopRefreshing: $stopRefreshing,
-            onRefresh: {
-                stopRefreshing = false
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                    stopRefreshing = true
+            content: {
+                LazyVStack {
+                    ForEach(1...20, id: \.self) { value in
+                        SampleCard(title: "\(value)")
+                            .paddingWhen(.top, 10) { value == 1 }
+                    }
+                }
+                .padding(.horizontal, 10)
+            },
+            subtitleContent: {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 5) {
+                        ForEach(Product.allCases, id: \.self) { entry in
+                            Button(action: {}) {
+                                Text(entry.rawValue)
+                                    .padding(.horizontal, 10)
+                                    .padding(.vertical, 5)
+                                    .background(Color.gray.opacity(0.1))
+                                    .cornerRadius(8)
+                            }
+                            .paddingWhen(.leading, 10) {
+                                entry == Product.allCases.first
+                            }
+                            .paddingWhen(.trailing, 10) {
+                                entry == Product.allCases.last
+                            }
+                        }
+                    }
+                }
+                .padding(.bottom, 10)
+            },
+            trailingBarItem: {
+                Button {
+                } label: {
+                    Image(systemName: "heart")
+                        .font(.system(size: 20, weight: .bold))
+                        .padding(.trailing, 10)
                 }
             }
         )
-    }
-
-    private var content: AnyView {
-        LazyVStack {
-            ForEach(1...2, id: \.self) { value in
-                SampleCard(title: "\(value)")
-                    .paddingWhen(.top, 10) { value == 1 }
-            }
-        }
-        .padding(.horizontal, 10)
-        .eraseToAnyView()
-    }
-
-    private var subtitleContent: AnyView {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 5) {
-                ForEach(Product.allCases, id: \.self) { entry in
-                    Text(entry.rawValue)
-                        .padding(.horizontal, 10)
-                        .padding(.vertical, 5)
-                        .background(BlurEffect(style: .systemMaterial))
-                        .clipShape(RoundedRectangle(cornerRadius: 8))
-                        .paddingWhen(.leading, 10) {
-                            entry == Product.allCases.first
-                        }
-                        .paddingWhen(.trailing, 10) {
-                            entry == Product.allCases.last
-                        }
-                }
-            }
-        }
-        .padding(.bottom, 10)
-        .eraseToAnyView()
-    }
-
-    private var primaryActionLabel: AnyView {
-        Button(
-            action: {},
-            label: {
-                Image(systemName: "heart")
-                    .font(.system(size: 20, weight: .bold))
-            }
-        )
-        .eraseToAnyView()
+        .navigationTitle("Title")
+        .refreshable(stopRefreshing: .constant(false), onRefresh: {})
     }
 }
 
@@ -105,10 +90,68 @@ struct SystemNavBar: View {
     }
 }
 
+struct SystemNavBarBackButton: View {
+    @State var isLinkActivated = false
+
+    var body: some View {
+        NavigationView {
+            NavigationLink(
+                isActive: $isLinkActivated,
+                destination: { destination }
+            ) {
+                Text("Go to")
+            }
+            .onAppear {
+                isLinkActivated.toggle()
+            }
+        }
+    }
+
+    var destination: some View {
+        VStack {
+            BackButton()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+
+    }
+}
+
+struct BackButton: View {
+    var action: (() -> Void)? = nil
+    var title: String? = nil
+
+    @Environment(\.presentationMode) var presentationMode
+
+    var body: some View {
+        Button(action: onTap) {
+            HStack(spacing: 5) {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(Color.accentColor)
+                    .font(.system(size: 22, weight: .medium))
+
+                Text(title ?? "Back")
+            }
+            .padding(.init(top: 5, leading: 8, bottom: 5, trailing: 5))
+
+            .foregroundColor(Color.accentColor)
+        }
+    }
+
+    private func onTap() {
+        if let _ = action {
+            action?()
+        } else {
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
 
         SystemNavBar().previewDisplayName("System")
+
+        SystemNavBarBackButton().previewDisplayName("Back")
     }
 }
