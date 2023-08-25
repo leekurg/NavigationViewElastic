f8fb99f61177a8c1e99e29a8303a2002970a11af
Ilya <105886145+leekurg@users.noreply.github.com>
2023-08-26 00:05:28 +0300

    Separated NavigationBarView and config (#5)
    * Separated navbad and config
    
    * Separated LargeTitleConfig
    
    ---------
    
    Co-authored-by: Илья Аникин <i.anikin@wallpaperscraft.com>

--- NavigationViewElastic/Sources/NavigationViewElastic/NavigationViewElastic.swift
+++ NavigationViewElastic/Sources/NavigationViewElastic/NavigationViewElastic.swift
@@ -14,6 +14,7 @@ import Foundation
 public struct NavigationViewElastic<C: View, S: View, P: View>: View {
     let title: String
     let blurStyle: UIBlurEffect.Style
+    let config: NavigationViewConfig
     @ViewBuilder let content: () -> C
     @ViewBuilder let subtitleContent: () -> S
     @ViewBuilder let trailingBarItem: () -> P
@@ -23,6 +24,7 @@ public struct NavigationViewElastic<C: View, S: View, P: View>: View {
     public init(
         title: String = "",
         blurStyle: UIBlurEffect.Style = .systemMaterial,
+        config: NavigationViewConfig = .init(),
         @ViewBuilder content: @escaping () -> C,
         @ViewBuilder subtitleContent: @escaping () -> S = { EmptyView() },
         @ViewBuilder trailingBarItem: @escaping () -> P = { EmptyView() },
@@ -31,6 +33,7 @@ public struct NavigationViewElastic<C: View, S: View, P: View>: View {
     ) {
         self.title = title
         self.blurStyle = blurStyle
+        self.config = config
         self.content = content
         self.subtitleContent = subtitleContent
         self.trailingBarItem = trailingBarItem
@@ -38,17 +41,8 @@ public struct NavigationViewElastic<C: View, S: View, P: View>: View {
         self.onRefresh = onRefresh
     }
 
-    private let largeTitleSupposedHeight: CGFloat = 40
-    private let largeTitleAdditionalTopPadding: CGFloat = 20
-    private let navigationViewLargeTitleTopPadding: CGFloat = 40
-    private let navigationViewLargeTitleBottomPadding: CGFloat = 5
-    private let progressTriggeringOffset: CGFloat = 30
-    private let progressViewTargetHeight: CGFloat = 60
-    private var heightToCover: CGFloat { largeTitleSupposedHeight + largeTitleAdditionalTopPadding }
-
     @State private var navigationViewSize: CGSize = .zero
     @State private var scrollOffset = CGPoint.zero
-    @State private var contentSize = CGSize.zero
     @State private var isRefreshing: Bool = false
 
     @Environment(\.colorScheme) var colorScheme
@@ -57,13 +51,12 @@ public struct NavigationViewElastic<C: View, S: View, P: View>: View {
         ZStack(alignment: .top) {
             ScrollViewObservable(showsIndicators: false, offset: $scrollOffset) {
                 content()
-                    .backgroundSizeReader(size: $contentSize)
-                    .padding(.top, navigationViewSize.height + heightToCover)
+                    .padding(.top, navigationViewSize.height + config.largeTitle.heightToCover)
             }
             .onChange(of: scrollOffset) { offset in
                 guard onRefresh != nil else { return }
 
-                if scrollOffset.isScrolledDown(progressViewTargetHeight + progressTriggeringOffset) {
+                if scrollOffset.isScrolledDown(config.progressViewTargetHeight + config.progressTriggeringOffset) {
                     if !isRefreshing {
                         UIImpactFeedbackGenerator(style: .light).impactOccurred()
                     }
@@ -81,18 +74,29 @@ public struct NavigationViewElastic<C: View, S: View, P: View>: View {
                 if refreshing { onRefresh?() }
             }
 
-            navigationView
+            NavigationBarView(
+                title: title,
+                blurStyle: blurStyle,
+                config: config,
+                scrollOffset: scrollOffset.y,
+                isRefreshable: onRefresh != nil,
+                isRefreshing: isRefreshing,
+                largeTitleLayerSize: $navigationViewSize,
+                subtitleContent: subtitleContent,
+                trailingBarItem: trailingBarItem
+            )
         }
         .ignoresSafeArea(.container, edges: .top)
     }
 }
 
-//// MARK: - Public API
+// MARK: - Public API
 public extension NavigationViewElastic {
     func refreshable(stopRefreshing: Binding<Bool>, onRefresh: @escaping () -> Void) -> Self {
-        NavigationViewElastic(
+        Self(
             title: self.title,
             blurStyle: self.blurStyle,
+            config: self.config,
             content: self.content,
             subtitleContent: self.subtitleContent,
             trailingBarItem: self.trailingBarItem,
@@ -102,9 +106,10 @@ public extension NavigationViewElastic {
     }
 
     func navigationTitle(_ title: String) -> Self {
-        NavigationViewElastic(
+        Self(
             title: title,
             blurStyle: self.blurStyle,
+            config: self.config,
             content: self.content,
             subtitleContent: self.subtitleContent,
             trailingBarItem: self.trailingBarItem,
@@ -114,9 +119,10 @@ public extension NavigationViewElastic {
     }
 
     func blurStyle(_ blurStyle: UIBlurEffect.Style) -> Self {
-        NavigationViewElastic(
+        Self(
             title: self.title,
             blurStyle: blurStyle,
+            config: self.config,
             content: self.content,
             subtitleContent: self.subtitleContent,
             trailingBarItem: self.trailingBarItem,
@@ -126,163 +132,42 @@ public extension NavigationViewElastic {
     }
 }
 
-// MARK: - Internal views
-private extension NavigationViewElastic {
-    var progressView: some View {
-        ActivityIndicator(isAnimating: isRefreshing) {
-            $0.hidesWhenStopped = false
-            $0.style = .large
-        }
-        .frame(maxWidth: .infinity)
-        .opacity(progressAppearFactor)
-        .opacity(1 - smallTitleOpacity)
-        .rotationEffect(.degrees(progressAppearFactor * 180))
-        .scaleEffect(clamp(progressAppearFactor - 0.2, min: 0.2))
-    }
+// MARK: - Config
+public struct NavigationViewConfig {
+    let largeTitle: LargeTitleConfig
+    let progressTriggeringOffset: CGFloat
+    let progressViewTargetHeight: CGFloat
 
-    var navigationView: some View {
-        ZStack(alignment: .top) {
-            largeTitleLayer
-
-            smallTitleLayer
-
-            progressView
-                .padding(.top, navigationViewLargeTitleTopPadding + 10)
-        }
-    }
-
-    // MARK: - large title
-    var largeTitleLayer: some View {
-        VStack(spacing: 0) {
-            Color.clear
-                .frame(height: scrollFactor)
-
-            VStack(spacing: 0) {
-                Text(title)
-                    .lineLimit(1)
-                    .font(.system(size: 32, weight: .bold)) //Do not change, a lot of depends on text size!
-                    .scaleEffect(largeTitleScale, anchor: .bottomLeading)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .opacity(largeTitleOpacity)
-                    .padding(
-                        .init(
-                            top: navigationViewLargeTitleTopPadding,
-                            leading: 20,
-                            bottom: navigationViewLargeTitleBottomPadding,
-                            trailing: 10
-                        )
-                    )
-
-                subtitleContent()
-
-                if isIntersectionWithContent {
-                    Divider()
-                }
-            }
-            .backgroundSizeReader(size: $navigationViewSize, firstValueOnly: true)
-        }
-        .background(largeTitleBackground)
-    }
-
-    // MARK: - small title
-    var smallTitleLayer: some View {
-        ZStack {
-            HStack { Text(title).lineLimit(1) }
-                .font(.system(size: 17, weight: .semibold))
-                .opacity(smallTitleOpacity)
-                .frame(maxWidth: UIScreen.width * 0.5)
-                .animation(.easeIn(duration: 0.2), value: smallTitleOpacity)
-
-            HStack {
-                Spacer()
-
-                trailingBarItem()
-                    .frame(maxWidth: UIScreen.width * 0.25, maxHeight: 30, alignment: .trailing)
-                    .clipped()
-            }
-        }
-        .frame(maxWidth: .infinity)
-        .padding(.bottom, 7)
-        .frame(
-            height: navigationViewLargeTitleTopPadding +
-            largeTitleSupposedHeight +
-            navigationViewLargeTitleBottomPadding,
-            alignment: .bottom
-        )
-        .background(smallTitleBackground)
+    public init(
+        largeTitleConfig: LargeTitleConfig = .init(),
+        progressTriggeringOffset: CGFloat = 30,
+        progressViewTargetHeight: CGFloat = 60
+    ) {
+        self.largeTitle = largeTitleConfig
+        self.progressTriggeringOffset = progressTriggeringOffset
+        self.progressViewTargetHeight = progressViewTargetHeight
     }
 }
 
-// MARK: - Computed props
-private extension NavigationViewElastic {
-    var smallTitleOpacity: CGFloat {
-        isReadyToCollapse ? 1 : 0
-    }
+public extension NavigationViewConfig {
+    struct LargeTitleConfig {
+        let supposedHeight: CGFloat
+        let topPadding: CGFloat
+        let additionalTopPadding: CGFloat
+        let bottomPadding: CGFloat
 
-    @ViewBuilder
-    var smallTitleBackground: some View {
-        if isIntersectionWithContent {
-            Color.clear
-        } else {
-            colorScheme == .dark ? Color.black : Color.white
+        var heightToCover: CGFloat { self.supposedHeight + self.additionalTopPadding }
+
+        public init(
+            supposedHeight: CGFloat = 40,
+            topPadding: CGFloat = 40,
+            additionalTopPadding: CGFloat = 15,
+            bottomPadding: CGFloat = 5
+        ) {
+            self.supposedHeight = supposedHeight
+            self.topPadding = topPadding
+            self.additionalTopPadding = additionalTopPadding
+            self.bottomPadding = bottomPadding
         }
-    }
-
-    var largeTitleOpacity: CGFloat {
-        isReadyToCollapse ? 0 : 1
-    }
-
-    @ViewBuilder
-    var largeTitleBackground: some View {
-        if isIntersectionWithContent {
-            BlurEffect(style: blurStyle)
-                .allowsHitTesting(false)    //fix blur intercepting some touches
-        } else {
-            colorScheme == .dark ? Color.black : Color.white
-        }
-    }
-
-    var largeTitleScale: CGFloat {
-        scrollOffset.isScrolledUp() ? 1.0 : clamp((-scrollOffset.y + 1000) / 1000.0, min: 1.0, max: 1.1 )
-    }
-
-    var scrollFactor: CGFloat {
-        if scrollOffset.isScrolledUp() {
-            return reduceScrollUpOffset(offsetY: scrollOffset.y, heightToCover: heightToCover)
-        } else {
-            return heightToCover + -scrollOffset.y
-        }
-    }
-
-    var progressAppearFactor: Double {
-        guard onRefresh != nil else { return 0 }
-        guard !isRefreshing else { return 1 }
-        guard scrollOffset.isScrolledDown() else { return 0 }
-
-        var offset: Double = clamp(
-            -scrollOffset.y,
-            min: progressTriggeringOffset,
-            max: progressTriggeringOffset + progressViewTargetHeight
-        )
-        offset -= progressTriggeringOffset
-        let opacity = clamp(offset / progressViewTargetHeight, min: 0, max: 1)
-
-        return opacity
-    }
-
-    var isIntersectionWithContent: Bool {
-        scrollFactor.isZero
-    }
-
-    var isReadyToCollapse: Bool {
-        scrollFactor < largeTitleAdditionalTopPadding
-    }
-}
-
-// MARK: - Service func
-private extension NavigationViewElastic {
-    func reduceScrollUpOffset(offsetY: CGFloat, heightToCover: CGFloat) -> CGFloat {
-        let offset = clamp(offsetY, min: 0, max: heightToCover)
-        return clamp(heightToCover - offset, min: 0)
     }
 }
